f3b3065656b76759c2544e11681cc14f02923c58
rkaucorp <rezaul.karim@springrain.io>
2022-06-14 13:15:35 +0600

    skeleton up

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { StateProvider } from "./store";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <StateProvider />
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
